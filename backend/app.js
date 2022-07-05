3d4425e1b31ebb52e773a9e078b919a665c2d861
abhimanyupapta <abhimanyupapta1@gmail.com>
2022-07-05 19:44:50 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -76,9 +76,9 @@ app.use("/api/v1/", cart);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 //middleware for errors
 app.use(errorMiddleware);
